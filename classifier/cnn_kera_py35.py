cbd6b9d381e9c07cbdb5c63cd0e7b5e84f7d91f7
Leye <wangleye@gmail.com>
2017-01-26 13:20:59 +0800

    Add ResNet Model

--- classifier/cnn_kera_py35.py
+++ classifier/cnn_kera_py35.py
@@ -1,10 +1,11 @@
 import dataLoader as loader
 import numpy as np
-from sklearn.cross_validation import KFold
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Convolution2D, AveragePooling2D, MaxPooling2D
 from keras.utils.visualize_util import plot
+from sklearn.model_selection import train_test_split
+from residual_blocks import building_residual_block
 
 
 def basicModel():
@@ -30,12 +31,12 @@ def basicModel():
 def CnnModel():
     """
     deep model structure:
-    conv 5*5*15 + averagepooling 2*2 + conv 5*5*15 + averagepooling 3*3 + flatten + dense + softmax
+    conv 4*4*6 + maxpooling 2*2 + conv 4*4*12 + maxpooling 2*2 + flatten + dense 30 + softmax
     """
     our_model = Sequential()
-    our_model.add(Convolution2D(5, 4, 4, activation='relu', input_shape=(24, 24, 1)))
+    our_model.add(Convolution2D(6, 4, 4, activation='relu', input_shape=(24, 24, 1)))
     our_model.add(MaxPooling2D(pool_size=(2, 2)))
-    our_model.add(Convolution2D(10, 4, 4, activation='relu'))
+    our_model.add(Convolution2D(12, 4, 4, activation='relu'))
     our_model.add(MaxPooling2D(pool_size=(2, 2)))
     our_model.add(Flatten())
     our_model.add(Dropout(0.25))
@@ -45,6 +46,32 @@ def CnnModel():
     our_model.compile(loss='categorical_crossentropy',
                       optimizer='adam', metrics=['accuracy'])
     plot(our_model, to_file='cnn_model.png')
+    return our_model
+
+
+def ResModel():
+    """
+    residual model structure:
+    input +
+    conv 3*3*6 + conv 3*3*6] * num_res_blocks +
+    flatten +
+    softmax
+    """
+    our_model = Sequential()
+    num_res_clocks = 4
+    kernel_size = (3, 3)
+    n_channels = 6
+    # build first layer
+    our_model.add(building_residual_block((24, 24, 1), n_channels, kernel_size))
+    print(our_model.output_shape)
+    for i in range(num_res_clocks - 1):
+        our_model.add(building_residual_block((24, 24, n_channels), n_channels, kernel_size))
+    our_model.add(Flatten())
+    our_model.add(Dropout(0.5))
+    our_model.add(Dense(2, activation='softmax'))
+    our_model.compile(loss='categorical_crossentropy',
+                      optimizer='adam', metrics=['accuracy'])
+    plot(our_model, to_file='res_model.png')
     return our_model
 
 
@@ -63,27 +90,22 @@ def construct_X_Y(evaluation_data):
     return X_data, Y_data
 
 
-def train(train_data, model, num_epoch):
-    X_train, Y_train = construct_X_Y(train_data)
-    model.fit(X_train, Y_train, batch_size=1,
+def train(X_train, Y_train, model, num_epoch):
+    model.fit(X_train, Y_train, batch_size=4,
               nb_epoch=num_epoch, verbose=1)
 
 
-def test(test_data, model):
-    X_test, Y_test = construct_X_Y(test_data)
+def test(X_test, Y_test, model):
     score = model.evaluate(X_test, Y_test, verbose=0)
     print(score)
 
 
 if __name__ == "__main__":
+    np.random.seed(123)  # expect reproduction, but Keras@Tensorflow still has problems (Keras Issue#2280)
     data = loader.mapFile()
-    kf = KFold(n=len(data.keys()), n_folds=5, shuffle=True)
-    for tr, tst in kf:
-        trk = np.asarray(list(data.keys()))[tr]
-        tstk = np.asarray(list(data.keys()))[tst]
-        train_data = dict((k, v) for k, v in data.items() if k in trk)
-        test_data = dict((k, v) for k, v in data.items() if k in tstk)
-        our_model = CnnModel()
-        for i in range(50):
-            train(train_data, our_model, 1)
-            test(test_data, our_model)
+    X_data, Y_data = construct_X_Y(data)
+    X_train, X_test, Y_train, Y_test = train_test_split(X_data, Y_data, test_size=0.4, random_state=123)
+    for i in range(10):
+        our_model = ResModel()
+        train(X_train, Y_train, our_model, 2)
+        test(X_test, Y_test, our_model)
